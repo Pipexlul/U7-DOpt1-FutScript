0c474c98a7f1d4f9798ea131db7a4d7e3216556a
Pipexlul <felipgg@gmail.com>
2023-05-14 19:30:03 -0400

    Add validator middleware to POST /equipos route

--- src/middleware/validators/routes.js
+++ src/middleware/validators/routes.js
@@ -1,8 +1,12 @@
 import validateLoginBody from "../body/loginBody.js";
+import validateTeamBody from "../body/teamBody.js";
 import validateAuth from "../authorization/jwt.js";
 
 const postLogin = [validateLoginBody];
-const postEquipos = ({ checkAdmin = false }) => [validateAuth({ checkAdmin })];
+const postEquipos = ({ checkAdmin = false }) => [
+  validateAuth({ checkAdmin }),
+  validateTeamBody,
+];
 
 export default {
   postLogin,
